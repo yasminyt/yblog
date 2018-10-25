ed9cf4df48f34609bba3c46837fe531ac5fc91fc
wzt3309 <wzt3309@gmail.com>
2018-10-25 22:17:19 +0800

    Add a new app categories

---
+++ backend/backend/local.py
@@ -0,0 +1,3 @@
+from backend.settings import *
+
+DEBUG = True
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 """backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/auth/', views.obtain_auth_token)
 ]
